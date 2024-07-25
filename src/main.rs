e37f10624e9a9cbef40ba0251efd6d6402423c70
Slavaqq <slavaqq@protonmail.com>
2024-07-25 16:06:52 +0200

    web server

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use std::{
     str::FromStr,
 };
 
-#[derive(Debug)]
+use serde::Serialize;
+use warp::http::StatusCode;
+use warp::{
+    reject::{Reject, Rejection},
+    Filter, Reply,
+};
+
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -11,8 +18,11 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
+
+#[derive(Debug)]
+struct InvalidId;
 
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -41,7 +51,7 @@ impl std::fmt::Display for QuestionId {
     }
 }
 
-impl std::str::FromStr for QuestionId {
+impl FromStr for QuestionId {
     type Err = std::io::Error;
 
     fn from_str(id: &str) -> Result<Self, Self::Err> {
@@ -52,12 +62,45 @@ impl std::str::FromStr for QuestionId {
     }
 }
 
-fn main() {
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
-        QuestionId::from_str("1").expect("Id not provided!"),
+        QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
         Some(vec!["faq".to_string()]),
     );
-    println!("{:#?}", question);
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid Id presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 00, 0, 1], 3030)).await;
 }
