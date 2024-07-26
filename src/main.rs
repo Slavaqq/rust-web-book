e6c75375eb84b2453d85e0591424b9009c42dd9e
Slavaqq <slavaqq@protonmail.com>
2024-07-26 14:06:27 +0200

    ndling CORS

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::{
 };
 
 use serde::Serialize;
-use warp::http::StatusCode;
+use warp::filters::cors::CorsForbidden;
+use warp::http::{Method, StatusCode};
 use warp::{
     reject::{Reject, Rejection},
     Filter, Reply,
@@ -79,14 +80,19 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(InvalidId) = r.find() {
+    if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid Id presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid Id presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
@@ -94,13 +100,18 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("not-in-the-request")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
     warp::serve(routes).run(([127, 00, 0, 1], 3030)).await;
 }
